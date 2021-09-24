d75a461c15075ab9487b57192ab58006f6ec0f6b
Javier Morales <javi_morales@hotmail.es>
2021-09-24 16:26:32 +0200

    Initial projects configuration done

--- Pico_Blink/build/pico-sdk/src/rp2040/cmake_install.cmake
+++ Pico_Blink/build/pico-sdk/src/rp2040/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/javi/pico/pico-sdk/src/rp2040
+# Install script for directory: C:/VSARM/sdk/pico/pico-sdk/src/rp2040
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/main")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,10 +32,15 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/VSARM/armcc/10 2021.07/bin/arm-none-eabi-objdump.exe")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/javi/pico/Pico_Projects/Pico_Blink/build/pico-sdk/src/rp2040/hardware_regs/cmake_install.cmake")
-  include("/home/javi/pico/Pico_Projects/Pico_Blink/build/pico-sdk/src/rp2040/hardware_structs/cmake_install.cmake")
+  include("D:/Users/Javi/GitHub/Pico_Projects/Pico_Blink/build/pico-sdk/src/rp2040/hardware_regs/cmake_install.cmake")
+  include("D:/Users/Javi/GitHub/Pico_Projects/Pico_Blink/build/pico-sdk/src/rp2040/hardware_structs/cmake_install.cmake")
 
 endif()
 
--- Pico_Hello_World/build/cmake_install.cmake
+++ Pico_Hello_World/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/javi/pico/Pico_Projects/Pico_Hello_World
+# Install script for directory: D:/Users/Javi/GitHub/Pico_Projects/Pico_Hello_World
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/main")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,9 +32,14 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/VSARM/armcc/10 2021.07/bin/arm-none-eabi-objdump.exe")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/javi/pico/Pico_Projects/Pico_Hello_World/build/pico-sdk/cmake_install.cmake")
+  include("D:/Users/Javi/GitHub/Pico_Projects/Pico_Hello_World/build/pico-sdk/cmake_install.cmake")
 
 endif()
 
@@ -46,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/javi/pico/Pico_Projects/Pico_Hello_World/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Users/Javi/GitHub/Pico_Projects/Pico_Hello_World/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
